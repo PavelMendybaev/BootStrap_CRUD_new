a5ffaff2ec39884047c878eaf3ef90a1a0682152
PavelMendybaev <72722409+PavelMendybaev@users.noreply.github.com>
2022-03-12 11:16:36 +0500

    Add files via upload

---
+++ PP_3_1_2_Boot_Security-master/target/classes/static/js/main.js
@@ -0,0 +1,28 @@
+const globalurl = "http://localhost:8080/api/admin";
+
+
+
+function sendRecuest(method , url , bady = null ){
+
+    const xhr = new XMLHttpRequest()
+
+    xhr.open(method , url)
+
+    xhr.responseType  = 'json'
+
+    xhr.onload = () => {
+        console.log(xhr.response);
+    }
+
+    xhr.send( JSON.stringify( bady));
+
+}
+
+
+body = {
+    login : 'test',
+    password : '1',
+    role : 'ADMIN'
+}
+
+sendRecuest('POST' , globalurl , body )
